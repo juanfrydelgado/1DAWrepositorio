b362263986312d69d5818c49e0b46744591c5203
Administrador <Administrador@JUANFRY>
2016-02-17 12:20:13 +0100

    los dos

--- Ejemreposit/src/Hola.java
+++ Ejemreposit/src/Hola.java
@@ -4,6 +4,7 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
    //hshshs
+		//fdsfsdfsfdsf
 	}
 
 }
--- Proyecto2egitremoto/src/Principal.java
+++ Proyecto2egitremoto/src/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
        System.out.println("hola");
        //jajajajaja
        //jeeeeeee
+       ///fdsfdsfsf
 	}
 
 }
